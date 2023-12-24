6016e6247ba6a2277f316522612603958f441fbc
Greg <cybercaptain@duck.com>
2023-12-24 08:42:00 +0200

    added bucket stack

--- squad44_dedicated_server/squad44_dedicated_server_stack.py
+++ squad44_dedicated_server/squad44_dedicated_server_stack.py
@@ -1,19 +1,20 @@
 from aws_cdk import (
     # Duration,
-    Stack,
+    Stack, RemovalPolicy,
     # aws_sqs as sqs,
 )
 from constructs import Construct
+from squad44_dedicated_server.storage.s3_stack import S3NestedStack
+
 
 class Squad44DedicatedServerStack(Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: Construct, construct_id: str,
+                 removal_policy: RemovalPolicy = RemovalPolicy.DESTROY,
+                 **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "Squad44DedicatedServerQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        bucket = S3NestedStack(
+            self, "S3NestedStack", auto_delete_objects=True, removal_policy=RemovalPolicy.DESTROY).bucket
+        
+        
